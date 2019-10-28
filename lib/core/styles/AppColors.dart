9ddaaca0d8db65fe9391673a43cfa7d8ed2aed7a
Kaan Biryol <kaan.biryol@adesso.com.tr>
2019-10-28 15:45:29 +0300

    theme updates

--- lib/core/styles/AppColors.dart
+++ lib/core/styles/AppColors.dart
@@ -5,8 +5,9 @@ class AppColors {
   static const background = Color.fromRGBO(255, 255, 255, 1.0);
   static const accent = Color.fromRGBO(49, 50, 54, 1.0);
   static const primaryIcon = Color.fromRGBO(49, 50, 54, 1.0);
-  static const accentIcon = Color.fromRGBO(49, 50, 54, 1.0);
   static const listDivider = Color.fromRGBO(0, 0, 0, 0.12);
+  static const secondaryAccent = Color.fromRGBO(30, 185, 128, 1.0);
+  static const secondaryBackground = Color.fromRGBO(52, 53, 56, 0.1);
 
   static const primaryTextColor = Color.fromRGBO(0, 0, 0, 0.87);
   static const secondaryTextColor = Color.fromRGBO(73, 74, 76, 1.0);
@@ -14,12 +15,14 @@ class AppColors {
   static const dark_primaryColor = Color.fromRGBO(32, 33, 37, 1.0);
   static const dark_background = Color.fromRGBO(32, 33, 37, 1.0);
   static const dark_accent = Color.fromRGBO(49, 50, 54, 1.0);
-  static const dark_primaryIcon = Color.fromRGBO(255, 255, 255, 1.0);
+  static const dark_primaryIcon = Color.fromRGBO(173, 173, 177, 1.0);
   static const dark_accentIcon = Color.fromRGBO(49, 50, 54, 1.0);
   static const dark_listDivider = Color.fromRGBO(49, 50, 54, 1.0);
 
   static const dark_primaryTextColor = Color.fromRGBO(255, 255, 255, 1);
   static const dark_secondaryTextColor = Color.fromRGBO(73, 74, 76, 1.0);
 
-  static const textAccent = Color.fromRGBO(201, 10, 10, 1.0);
+  static const dark_commentsPageDivider = Color.fromRGBO(18, 18, 18, 0.3);
+  static const dark_secondaryAccent = Color.fromRGBO(4, 93, 86, 1.0);
+  static const dark_secondaryBackground = Color.fromRGBO(52, 53, 56, 0.4);
 }
